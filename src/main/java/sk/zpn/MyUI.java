01abc137eedb9dc0644f59fdfa8e817471a047b0
Martin <mmartin33@gmail.com>
2019-01-20 08:46:13 +0100

    login

--- src/main/java/sk/zpn/MyUI.java
+++ src/main/java/sk/zpn/MyUI.java
@@ -6,10 +6,12 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.navigator.NavigationStateManager;
 import com.vaadin.navigator.Navigator;
+import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.server.VaadinSession;
 import com.vaadin.ui.*;
+import sk.zpn.zaklad.model.Pripojenie;
 import sk.zpn.zaklad.model.UzivatelNastroje;
 import sk.zpn.zaklad.view.LoginView;
 import sk.zpn.zaklad.view.MainView;
@@ -24,17 +26,23 @@ import sk.zpn.zaklad.view.MainView;
 @Theme("mytheme")
 public class MyUI extends UI {
 
-    Navigator navigator;
+    public Navigator navigator;
     LoginView login;
-
+    MainView  mainView;
+    Pripojenie p;
     @Override
     protected void init(VaadinRequest vaadinRequest) {
+        p =new Pripojenie();
+
         login = new LoginView();
+        mainView=new MainView(navigator);
+
         login.addLoginListener(e-> {
             String name = e.getLoginParameter("username");
             String pass = e.getLoginParameter("password");
 
             if (UzivatelNastroje.overUzivatela(name,pass)) {
+                System.out.println("uzivatel overeny");
                 VaadinSession.getCurrent().setAttribute("uzivatel","name");
                 navigator.navigateTo(MainView.NAME);
             }
@@ -44,17 +52,59 @@ public class MyUI extends UI {
 
 
         navigator = new Navigator(this, this);
-        navigator.addView(MainView.NAME, MainView.class);
+        VaadinSession.getCurrent().setAttribute("navigator","navigator");
+        navigator.addView(MainView.NAME, mainView);
         navigator.addView(LoginView.NAME, login);
+        //navigator.navigateTo(LoginView.NAME);
         navigator.navigateTo(MainView.NAME);
+
+        navigator.addViewChangeListener(new ViewChangeListener() {
+            @Override
+            public boolean beforeViewChange(ViewChangeEvent event) {
+
+                if (jeUzivatelPrihlaseny()) {
+                    System.out.println("je prihlaseny");
+                    navigator.navigateTo(MainView.NAME);
+                    return false;
+                }
+                else{
+                    navigator.navigateTo(LoginView.NAME);
+                    return false;
+                   }
+
+            }
+        });
+
+
+        //navigator.navigateTo(LoginView.NAME);
     }
 
     public final static boolean jeUzivatelPrihlaseny() {
-        return VaadinSession.getCurrent().getAttribute("user") != null;
+        if (VaadinSession.getCurrent().getAttribute("uzivatel")==null)
+            System.out.println("Nie je prihlaseny ziadny uzivatel");
+        return VaadinSession.getCurrent().getAttribute("uzivatel") != null;
+    }
+
+    public static MyUI get() {
+        UI ui = UI.getCurrent();
+        if (ui instanceof MyUI) {
+            return (MyUI) ui;
+        }
+        return null;
     }
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
     @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
     public static class MyUIServlet extends VaadinServlet {
+    }
+
+    @Override
+    public Navigator getNavigator() {
+        return navigator;
+    }
+
+    @Override
+    public void setNavigator(Navigator navigator) {
+        this.navigator = navigator;
     }
 }
