4a3a4a033e37629688c12b34d4c5b76d31056f5b
mmartin33 <mmartin33@gmail.com>
2019-03-20 20:34:29 +0100

    duble to Bigdecimal2

--- src/main/java/sk/zpn/zaklad/model/ProduktyNastroje.java
+++ src/main/java/sk/zpn/zaklad/model/ProduktyNastroje.java
@@ -25,6 +25,23 @@ public class ProduktyNastroje {
         return q.getResultList();
     }
 
+    public static Optional<Produkt> prvyProduktPodlaNazvu(String nazov){
+        EntityManager em = (EntityManager) VaadinSession.getCurrent().getAttribute("createEntityManager");
+        TypedQuery<Produkt> q = em.createNamedQuery("Produkt.getPodlaNazvu", Produkt.class)
+                .setParameter("nazov", nazov);
+        List<Produkt> produkty = q.getResultList();
+        return produkty.size() > 0 ? Optional.of(q.getResultList().get(0)) : Optional.empty();
+    }
+    public static boolean uzExistujeKat(String kat){
+        EntityManager em = (EntityManager) VaadinSession.getCurrent().getAttribute("createEntityManager");
+        TypedQuery<Produkt> q = em.createNamedQuery("Produkt.getPodlaKodu", Produkt.class)
+                .setParameter("kat", kat);
+        List<Produkt> produkty = q.getResultList();
+        return (produkty.size() > 0) ;
+    }
+
+
+
     public static Produkt ulozProdukt(Produkt f){
         EntityManager em = (EntityManager) VaadinSession.getCurrent().getAttribute("createEntityManager");
         if (f.isNew())
